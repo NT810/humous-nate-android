996bdaf89f7bb9c12414593c1e0080a482ea854b
MadBear422 <supermarioland100@gmail.com>
2021-11-14 21:08:49 -0800

    epic sauce

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -60,7 +60,7 @@ class MainMenuState extends MusicBeatState
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
 		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		bg.scrollFactor.set(0, yScroll);
-		bg.setGraphicSize(Std.int(bg.width * 1.175));
+		bg.setGraphicSize(Std.int(bg.width * 1.13));
 		bg.updateHitbox();
 		bg.screenCenter();
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
